fbcf594ab4296c95aebed3fc4ee150d9cf456122
chandushine <113595994+chandushine@users.noreply.github.com>
2023-01-02 23:05:28 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker image build -t nopcommerce:1.44 .
 docker images ls
-docker container run -d --name chandu144 -P nopcommerce:1.44
+docker container run -d --name chandu145 -P nopcommerce:1.44
 docker container ls -a
